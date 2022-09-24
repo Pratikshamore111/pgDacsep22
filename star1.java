10023c2bc5aee220e8d062e3c2d5de989bf58b10
Pratikshamore111 <113437907+Pratikshamore111@users.noreply.github.com>
2022-09-24 17:21:27 +0530

    Add files via upload

---
+++ star1.java
@@ -0,0 +1,35 @@
+class star1{
+public static void main(String[] args)
+{
+   for(int i=1; i<5; i++)//row
+    {
+	   for(int j=5; j>i; j--)//space
+	    {
+	   System.out.print("  ");
+		}
+	   for(int j=1; j<i; j++)//col
+	   {
+	    System.out.print("* ");
+	   }
+	   System.out.println();
+	}
+  for(int i=1; i<=5; i++)//rows
+  {
+	  for(int j=1; j<=i; j++)//space
+	  {
+          System.out.print("  ");
+	  }
+	  for(int j=5; j>i; j--)//col
+	  {
+          System.out.print("* ");
+	  }
+	  System.out.println();
+  }
+}
+}
+
+  
+ 
+		
+	
+
